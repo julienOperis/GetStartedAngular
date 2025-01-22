5e85a0a92f86145058396a42f33cc84431343b5f
OPERIS\julien.boulay <julien.boulay@operis.fr>
2025-01-22 17:10:17 +0100

    correction showAlert

--- front/src/app/core/services/alert.service.ts
+++ front/src/app/core/services/alert.service.ts
@@ -7,8 +7,7 @@ import { BehaviorSubject } from 'rxjs';
 
 
 export class AlertService {
-
-  private _showAlert = new BehaviorSubject<{message:string,type:string} | null>(null);
+  private _showAlert = new BehaviorSubject<{message:string,type:string}| null>(null);  
   public showAlert$ = this._showAlert.asObservable();
 
   public setAlert(message:string,type:string):void{
